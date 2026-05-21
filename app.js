e9d50d98e884d56010e32083388ca5ea7ec7eaf1
PermataAyu <unocondo@gmail.com>
2026-05-21 13:11:05 +0700

    fix health

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 const start = async () => {
-  await app.listen(PORT+1)
+  await app.listen(PORT)
   console.log(`server started on port ${PORT}`)
 }
 
